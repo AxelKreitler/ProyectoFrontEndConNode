bbb978a2a66d60ef2b810c2a9532462de04e9885
Axel Kreitler <AxelKreitler@hotmail.com>
2018-11-28 21:07:58 -0300

    Index now shows the popular posts.

--- Proyecto/routes/index.js
+++ Proyecto/routes/index.js
@@ -5,11 +5,17 @@ var db = require('./db');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var queryPublicaciones = "select * from publicaciones order by id desc";
+  var queryMasVistos = "select * from publicaciones order by vistas desc limit 5";
   db.query(queryPublicaciones, function(error,result){
     if(error){
       console.log(error);
     }
-    res.render('index', { publicaciones:result , idU: req.session.idU});
+    db.query(queryMasVistos, function(error,resultado){
+      if(error){
+        console.log(error);
+      }
+      res.render('index', { publicaciones:result ,populares: resultado, idU: req.session.idU});
+    });
   });
 });
 
