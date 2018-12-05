cc5463317cc4176e66c89e721c8e573223959315
DidierSoules <36740732+DidierSoules@users.noreply.github.com>
2018-12-05 00:49:12 -0300

    Termine las vistas de GAMES. Modifique about us y post populares.

--- Proyecto/routes/new-project.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req,res,next){
-    res.render('new-project',{idU: req.session.idU});
-});
-
-module.exports = router;
